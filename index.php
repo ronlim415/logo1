951fec2499e3bc58c904dc373e452fb4df71f6fd
ronlim415 <60123575+ronlim415@users.noreply.github.com>
2020-09-10 21:45:04 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-<!DOCTYPE html>
-<html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -19,6 +17,5 @@
   <div class="rbrow"></div>
   <div class="lbrow"></div>
   <div class="beak"></div>
-  <div class="triangle"></div>
 </body>
 </html>
